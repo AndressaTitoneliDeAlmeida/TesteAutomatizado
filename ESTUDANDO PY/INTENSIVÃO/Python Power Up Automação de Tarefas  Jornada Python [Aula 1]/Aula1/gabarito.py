da331947be6541e559028f447e19fbc13a319514
AndressaTitoneliDeAlmeida <139380309+AndressaTitoneliDeAlmeida@users.noreply.github.com>
2023-09-27 20:55:51 -0300

    envio de arquivo

--- ESTUDANDO PY/INTENSIVÃO/Python Power Up Automação de Tarefas  Jornada Python [Aula 1]/Aula1/gabarito.py
+++ ESTUDANDO PY/INTENSIVÃO/Python Power Up Automação de Tarefas  Jornada Python [Aula 1]/Aula1/gabarito.py
@@ -9,29 +9,28 @@ import time
 # pyautogui.press -> apertar 1 tecla
 # pyautogui.click -> clicar em algum lugar da tela
 # pyautogui.hotkey -> combinação de teclas
-pyautogui.PAUSE = 0.2
+pyautogui.PAUSE = 0.3
 
 # abrir o navegador (chrome)
 pyautogui.press("win")
-pyautogui.write("Chrome")
+pyautogui.write("chrome")
 pyautogui.press("enter")
 
 # entrar no link 
 pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
 pyautogui.press("enter")
-time.sleep(2)
+time.sleep(3)
 
 
 # Passo 2: Fazer login
 # selecionar o campo de email
-pyautogui.click(x=642, y=549)
+pyautogui.click(x=685, y=451)
 # escrever o seu email
 pyautogui.write("pythonimpressionador@gmail.com")
 pyautogui.press("tab") # passando pro próximo campo
-pyautogui.write("mudar321")
-pyautogui.click(x=648, y=687) # clique no botao de login
-pyautogui.press("enter")
-time.sleep(2)
+pyautogui.write("sua senha")
+pyautogui.click(x=955, y=638) # clique no botao de login
+time.sleep(3)
 
 # Passo 3: Importar a base de produtos pra cadastrar
 import pandas as pd
@@ -43,7 +42,7 @@ print(tabela)
 # Passo 4: Cadastrar um produto
 for linha in tabela.index:
     # clicar no campo de código
-    pyautogui.click(x=913, y=774)
+    pyautogui.click(x=653, y=294)
     # pegar da tabela o valor do campo que a gente quer preencher
     codigo = tabela.loc[linha, "codigo"]
     # preencher o campo
